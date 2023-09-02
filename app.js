08ab44f552bc916b748b930fd4f98a023cb1f5d1
Talal Syed <3298457+stalal@users.noreply.github.com>
2023-09-02 17:42:23 -0400

    Everything working properly!

--- app.js
+++ app.js
@@ -1,11 +1,35 @@
-let a = 0;
-let b = 0;
-let operator;
-let result = 0;
+// let a = 0;
+// let b = 0;
+// let operator;
+// let result = 0;
 
-function operate(a,b,operator){
-
-}
+function operate(a, b, operator) {
+    console.log(a, b, operator);
+    
+    switch (operator) {
+      case "+":
+            displayText.textContent = Number((Number(a)+Number(b)).toFixed(2));
+            firstNumber = displayText.textContent;
+        break;
+      case "–": 
+            displayText.textContent = Number((Number(a)-Number(b)).toFixed(2));
+            firstNumber = displayText.textContent;       
+        break;
+      case "*":
+            displayText.textContent = Number((Number(a)*Number(b)).toFixed(2));
+            firstNumber = displayText.textContent;        
+        break;
+      case "/":
+            if(Number(b)!=0){
+                displayText.textContent = Number((Number(a)/Number(b)).toFixed(2));
+                firstNumber = displayText.textContent;    
+            }    
+            else displayText.textContent = "You can't divide by 0 🙄";
+        break;
+      default:
+        break;
+    }
+  }
 
 function add(a,b){
     return a+b;
@@ -24,12 +48,17 @@ function divide(a,b){
 }
 
 let numbers = document.querySelectorAll(".numbers");
+let operations = document.querySelectorAll(".operations");
+
 let displayText = document.querySelector(".display");
+let calculationHistory = document.querySelector(".calculation");
+
 let deleteButton = document.querySelector(".delete");
 let clearButton = document.querySelector(".clear");
+let equalButton = document.querySelector(".equal-button");
 
-let firstNumber = 0;
-let secondNumber = 0;
+let firstNumber = "blank";
+let secondNumber = "blank";
 let operation = "";
 
 clearText();
@@ -41,9 +70,31 @@ numbers.forEach((number)=>{
     });
 });
 
+equalButton.addEventListener("click", () => {
+    if(firstNumber != "blank"){
+        secondNumber = displayText.textContent;
+        calculationHistory.textContent += displayText.textContent;
+        operate(firstNumber, secondNumber, operation);
+    }
+});
+
+operations.forEach((op)=>{
+    op.addEventListener("click", () => {   
+        if(firstNumber == "blank") firstNumber = displayText.textContent;
+        else operate(firstNumber, displayText.textContent, operation);
+        
+        calculationHistory.textContent = displayText.textContent + op.textContent;
+        displayText.textContent = "";
+        operation = op.textContent;
+    });
+});
+
 function clearText(){
     clearButton.addEventListener("click", () => {
-        displayText.textContent = " ";
+        displayText.textContent = "";
+        calculationHistory.textContent = "";
+        firstNumber = "blank";
+        secondNumber = "blank";
     });
 }
 
