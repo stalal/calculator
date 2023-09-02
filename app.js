6788fd2174dd5f1470bb2a73f81be35d15722dbe
Talal Syed <3298457+stalal@users.noreply.github.com>
2023-09-02 14:45:42 -0400

    Built the html structure of the calculator and set up blank JS functions

---
+++ app.js
@@ -0,0 +1,25 @@
+let a = 0;
+let b = 0;
+let operator;
+let result = 0;
+
+
+function operate(a,b,operator){
+
+}
+
+function add(a,b){
+    return a+b;
+}
+
+function subtract(a,b){
+    return a-b;
+}
+
+function multiply(a,b){
+    return a*b;
+}
+
+function divide(a,b){
+    return a/b;
+}
